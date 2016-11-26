c44970388fcd791b99cc9fc433ea77a5125d70cd
CraftedCart <CraftedCart@users.noreply.github.com>
2016-11-26 14:48:42 +0000

    Added icons

--- src/main/java/craftedcart/gcisomanager/GCISOManager.java
+++ src/main/java/craftedcart/gcisomanager/GCISOManager.java
@@ -6,6 +6,8 @@ import craftedcart.gcisomanager.util.LogHelper;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -14,6 +16,9 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author CraftedCart
@@ -22,7 +27,10 @@ import java.io.IOException;
 public class GCISOManager extends Application {
 
     private ISOManager isoManager = new ISOManager();
+
     private TreeView<String> treeView;
+
+    private Map<String, Image> imageMap;
 
     public static void main(String[] args) {
         Application.launch(GCISOManager.class, args);
@@ -31,6 +39,8 @@ public class GCISOManager extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         LogHelper.info(GCISOManager.class, "GC ISO Manager Launched");
+
+        cacheImages();
 
         //The root of the scene shown in the main window
         BorderPane root = new BorderPane();
@@ -50,6 +60,22 @@ public class GCISOManager extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle(LangManager.getItem("gcISOManager"));
         primaryStage.show();
+    }
+
+    private void cacheImages() {
+        imageMap = new HashMap<>();
+
+        imageMap.put("code-braces", new Image(getClass().getResourceAsStream("/image/code-braces.png")));
+        imageMap.put("cube", new Image(getClass().getResourceAsStream("/image/cube.png")));
+        imageMap.put("disk", new Image(getClass().getResourceAsStream("/image/disk.png")));
+        imageMap.put("file-document", new Image(getClass().getResourceAsStream("/image/file-document.png")));
+        imageMap.put("file-image", new Image(getClass().getResourceAsStream("/image/file-image.png")));
+        imageMap.put("file", new Image(getClass().getResourceAsStream("/image/file.png")));
+        imageMap.put("folder-outline", new Image(getClass().getResourceAsStream("/image/folder-outline.png")));
+        imageMap.put("human", new Image(getClass().getResourceAsStream("/image/human.png")));
+        imageMap.put("image-multiple", new Image(getClass().getResourceAsStream("/image/image-multiple.png")));
+        imageMap.put("music-box", new Image(getClass().getResourceAsStream("/image/music-box.png")));
+        imageMap.put("zip-box", new Image(getClass().getResourceAsStream("/image/zip-box.png")));
     }
 
     private void chooseISO(Stage stage) {
@@ -92,9 +118,37 @@ public class GCISOManager extends Application {
             parent.getChildren().add(item);
         }
 
+        ImageView image = new ImageView(getImageForFilename(node.getData().filename, node.getData().isDir));
+        image.setFitHeight(20);
+        image.setFitWidth(20);
+
+        item.setGraphic(image);
+
         for (Tree.Node<FileEntry> iNode : node.getChildren()) {
             parent = item;
             recursePopulateFileEntryTree(iNode, parent);
+        }
+    }
+
+    private Image getImageForFilename(String filename, boolean isDir) {
+        if (filename.equals("/")) return imageMap.get("disk");
+        if (filename.toUpperCase().endsWith(".REL")) return imageMap.get("code-braces");
+        if (filename.toUpperCase().endsWith(".DOL")) return imageMap.get("code-braces");
+        if (filename.toUpperCase().endsWith(".ELF")) return imageMap.get("code-braces");
+        if (filename.toUpperCase().endsWith(".ADP")) return imageMap.get("music-box");
+        if (filename.toUpperCase().endsWith(".DSP")) return imageMap.get("music-box");
+        if (filename.toUpperCase().endsWith(".GMA")) return imageMap.get("cube");
+        if (filename.toUpperCase().endsWith(".TPL")) return imageMap.get("image-multiple");
+        if (filename.toUpperCase().endsWith(".LZ")) return imageMap.get("zip-box");
+        if (filename.toUpperCase().endsWith(".SKL")) return imageMap.get("human");
+        if (filename.toUpperCase().endsWith(".BNR")) return imageMap.get("file-image");
+        if (filename.toUpperCase().endsWith(".STR")) return imageMap.get("file-document");
+
+        //No other icon found
+        if (isDir) {
+            return imageMap.get("folder-outline");
+        } else {
+            return imageMap.get("file");
         }
     }
 
